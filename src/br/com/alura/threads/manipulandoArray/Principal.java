bbcc71c0f8ca171a1507224472d0a3df5f22f2b6
henrique.lima <henrique.lima@valecard.com.br>
2023-03-04 19:54:07 -0300

    5.2 Implementação com Vector: é thread-safe, logo não tivemos problema com index

--- src/br/com/alura/threads/manipulandoArray/Principal.java
+++ src/br/com/alura/threads/manipulandoArray/Principal.java
@@ -1,12 +1,12 @@
 package br.com.alura.threads.manipulandoArray;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 public class Principal {
 
     public static void main(String[] args) {
-        List<String> lista = new ArrayList<>();
+        List<String> lista = new Vector<>();
 
         for(int i=0;i<10;i++){
             new Thread(new TarefaAdicionaElemento(lista,i)).start();
@@ -15,11 +15,5 @@ public class Principal {
         for(int i=0;i<lista.size();i++){
             System.out.println("lista[" + i + "] = " + lista.get(i));
         }
-//      output:
-//        lista[992] = Thread 9 - 96
-//        lista[993] = Thread 9 - 97
-//        lista[994] = Thread 9 - 98
-//        lista[995] = Thread 9 - 99
-//        deveria ir até o index 999, mas arrayList não é thread-safe
     }
 }
